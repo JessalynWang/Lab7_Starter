9c3e013378fcf005551d158a5ab48a51ce694091
Jessalyn Wang <jess4lynwang@gmail.com>
2022-11-08 17:13:40 -0800

    changing root for deployment

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -58,7 +58,7 @@ function initializeServiceWorker() {
     window.addEventListener('load', async (_) => {
       try {
         const registration = await navigator.serviceWorker.register("/sw.js", {
-          scope: "/",
+          scope: "Lab7_Starter/",
         });
         if (registration.active) {
           console.log("Service worker installed and active");
